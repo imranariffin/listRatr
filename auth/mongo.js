34fba2b3012b376dee83a45aa07eaf7dcd9af801
imranariffin <imranariffin@yahoo.com>
2015-09-17 18:21:23 -0400

    rem folder auth for git push publicize

--- auth/mongo.js
+++
@@ -1,3 +0,0 @@
-// exports.MONGO_URI = 'mongodb://imranariffin:katakuncibaru1993@ds045027.mongolab.com:45027/dealters_dev';
-
-exports.MONGO_URI = 'mongodb://heroku_6znjq7ck:7rndl9oriu84jao3fgtte0so35@ds035623.mongolab.com:35623/heroku_6znjq7ck'
